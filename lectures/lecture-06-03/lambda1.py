08e2c61d544a8f3ea9fe3509b3be6f0274ca27bd
Hongwei <gmhwxi@gmail.com>
2025-06-03 15:33:52 -0400

    Updating: adding lecture-06-03

---
+++ lectures/lecture-06-03/lambda1.py
@@ -0,0 +1,327 @@
+# datatype tval =
+# | TVint of int
+# | TVbtf of bool
+# | TVclo of (term, xenv)
+
+# datatype xenv =
+# | EVnil of ()
+# | EVcons of (strn, tval, xenv)
+
+# datatype term =
+# | TMint of int
+# | TMbtf of bool
+# | TMvar of strn
+# | TMlam of (strn, term)
+# | TMapp of (term, term)
+# | TMopr of (strn(*opr*), list(term))
+# | TMif0 of (term, term, term)
+# | TMfix of (strn(*f*), strn(*x*), term)
+
+class term:
+    ctag = ""
+    def __str__(self):
+        return ("term(" + self.ctag + ")")
+# end-of-class(term)
+
+class term_int(term):
+    def __init__(self, arg1):
+        self.arg1 = arg1
+        self.ctag = "TMint"
+    def __str__(self):
+        return ("TMint(" + str(self.arg1) + ")")
+# end-of-class(term_int(term))
+
+class term_btf(term):
+    def __init__(self, arg1):
+        self.arg1 = arg1
+        self.ctag = "TMbtf"
+    def __str__(self):
+        return ("TMbtf(" + str(self.arg1) + ")")
+# end-of-class(term_btf(term))
+
+class term_var(term):
+    def __init__(self, arg1):
+        self.arg1 = arg1
+        self.ctag = "TMvar"
+    def __str__(self):
+        return ("TMvar(" + self.arg1 + ")")
+# end-of-class(term_var(term))
+
+class term_lam(term):
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.ctag = "TMlam"
+    def __str__(self):
+        return ("TMlam(" + self.arg1 + ";" + str(self.arg2) + ")")
+# end-of-class(term_lam(term))
+
+class term_app(term):
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.ctag = "TMapp"
+    def __str__(self):
+        return ("TMapp(" + str(self.arg1) + ";" + str(self.arg2) + ")")
+# end-of-class(term_app(term))
+
+##################################################################
+
+class term_opr(term):
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.ctag = "TMopr"
+    def __str__(self):
+        return ("TMopr(" + self.arg1 + ";" + str(self.arg2) + ")")
+# end-of-class(term_opr(term))
+
+class term_if0(term):
+    def __init__(self, arg1, arg2, arg3):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+        self.ctag = "TMif0"
+    def __str__(self):
+        return ("TMif0(" + str(self.arg1) + ";" + str(self.arg2) + ";" + str(self.arg3) + ")")
+# end-of-class(term_if0(term))
+
+class term_fix(term):
+    def __init__(self, arg1, arg2, arg3):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+        self.ctag = "TMfix"
+    def __str__(self):
+        return ("TMfix(" + self.arg1 + ";" + self.arg2 + ";" + str(self.arg3) + ")")
+# end-of-class(term_fix(term))
+
+##################################################################
+
+class tval:
+    ctag = ""
+    def __str__(self):
+        return ("tval(" + self.ctag + ")")
+# end-of-class(tval)
+
+class tval_int(tval):
+    def __init__(self, arg1):
+        self.arg1 = arg1
+        self.ctag = "TVint"
+    def __str__(self):
+        return ("TVint(" + str(self.arg1) + ")")
+# end-of-class(tval_int(tval))
+
+class tval_btf(tval):
+    def __init__(self, arg1):
+        self.arg1 = arg1
+        self.ctag = "TVbtf"
+    def __str__(self):
+        return ("TVbtf(" + str(self.arg1) + ")")
+# end-of-class(tval_btf(tval))
+
+class tval_clo(tval):
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.ctag = "TVclo"
+    def __str__(self):
+        return ("TVclo(" + str(self.arg1) + str(self.arg2) + ")")
+# end-of-class(tval_clo(tval))
+
+##################################################################
+
+# datatype xenv =
+# | EVnil of ()
+# | EVcons of (strn, tval, xenv)
+
+class xenv:
+    ctag = ""
+    def __str__(self):
+        return ("xenv(" + self.ctag + ")")
+# end-of-class(xenv)
+
+class xenv_nil(xenv):
+    def __init__(self):
+        self.ctag = "EVnil"
+    def __str__(self):
+        return ("EVnil(" + ")")
+# end-of-class(xenv_nil(xenv))
+
+class xenv_cons(xenv):
+    def __init__(self, arg1, arg2, arg3):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+        self.ctag = "EVcons"
+    def __str__(self):
+        return ("EVcons(" + self.arg1 + ";" + str(self.arg2) + ";" + str(self.arg3) + ")")
+# end-of-class(xenv_cons(xenv))
+
+##################################################################
+
+var_x = term_var("x")
+var_y = term_var("y")
+var_f = term_var("f")
+var_n = term_var("n")
+int_0 = term_int( 0 )
+int_1 = term_int( 1 )
+btf_t = term_btf(True)
+btf_f = term_btf(False)
+term_I = term_lam("x", var_x) # lam x. x
+print("term_I = " + str(term_I))
+term_K = term_lam("x", term_lam("y", var_x)) # lam x. lam y. x
+term_K1 = term_lam("x", term_lam("y", var_y)) # lam x. lam y. y
+print("term_K = " + str(term_K))
+print("term_K1 = " + str(term_K1))
+
+term_add = lambda a1, a2: term_opr("+", [a1, a2])
+term_sub = lambda a1, a2: term_opr("-", [a1, a2])
+term_mul = lambda a1, a2: term_opr("*", [a1, a2])
+term_lt = lambda a1, a2: term_opr("<", [a1, a2])
+term_gt = lambda a1, a2: term_opr(">", [a1, a2])
+term_eq = lambda a1, a2: term_opr("=", [a1, a2])
+term_lte = lambda a1, a2: term_opr("<=", [a1, a2])
+term_gte = lambda a1, a2: term_opr(">=", [a1, a2])
+term_neq = lambda a1, a2: term_opr("!=", [a1, a2])
+term_cmp = lambda a1, a2: term_opr("cmp", [a1, a2])
+
+term_fact = term_fix("f", "n", term_if0(term_lte(var_n, int_0), int_1, term_mul(var_n, term_app(var_f, term_sub(var_n, int_1)))))
+
+print("term_fact = " + str(term_fact))
+
+def term_subst(tm0, x00, sub):
+    def subst(tm0):
+        return term_subst(tm0, x00, sub)
+    # |TMvar(x1) =>
+    #  if x0 = x1 then u0 else t0
+    if (tm0.ctag == "TMvar"):
+        x01 = tm0.arg1
+        # print("term_subst: TMvar")
+        # print("x00 = " + x00)
+        # print("x01 = " + x01)
+        return sub if (x00==x01) else tm0
+    # |TMlam(x1, t1) =>
+    #  if x0 = x1
+    #  then t0 else TMlam(x1, term_subst(t1, x0, u0))
+    if (tm0.ctag == "TMlam"):
+        x01 = tm0.arg1
+        tm1 = tm0.arg2
+        return tm0 if (x00==x01) else term_lam(x01, subst(tm1))
+    # |TMapp(t1, t2) =>
+    #  TMapp(term_subst(t1, x0, u0), term_subst(t2, x0, u0))
+    if (tm0.ctag == "TMapp"):
+        tm1 = tm0.arg1
+        tm2 = tm0.arg2
+        return term_app(subst(tm1), subst(tm2))
+    raise TypeError(tm0) # HX-2025-05-27: should be deadcode!
+
+# test_tm1 = term_subst(var_x, "x", term_I)
+# print("test_tm1 = " + str(test_tm1))
+# test_tm2 = term_subst(var_x, "y", term_I)
+# print("test_tm2 = " + str(test_tm2))
+# test_tm3 = term_subst(term_app(var_x, var_y), "y", term_I)
+# print("test_tm3 = " + str(test_tm3))
+
+##################################################################
+
+# #extern
+# fun
+# term_eval00(tm0: term): tval
+# #extern
+# fun
+# term_eval01(tm0: term, env: envx): tval
+
+def term_eval00(tm0):
+    return term_eval01(tm0, xenv_nil())
+
+##################################################################
+
+def xenv_search(env, x00):
+    if env.ctag == "EVnil":
+        return None
+    if env.ctag == "EVcons":
+        if env.arg1 == x00:
+            return env.arg2
+        else:
+            return xenv_search(env.arg3, x00)
+    raise TypeError(env) # HX-2025-06-03: deadcode!
+
+##################################################################
+        
+def term_eval01(tm0, env):
+    if (tm0.ctag == "TMint"):
+        return tval_int(tm0.arg1)
+    if (tm0.ctag == "TMbtf"):
+        return tval_btf(tm0.arg1)
+    if (tm0.ctag == "TMlam"):
+        return tval_clo(tm0, env)
+    if (tm0.ctag == "TMfix"):
+        return tval_clo(tm0, env)
+    if (tm0.ctag == "TMvar"):
+        tv0 = xenv_search(env, tm0.arg1)
+        assert tv0 is not None
+        return tv0
+    if (tm0.ctag == "TMopr"):
+        pnm = tm0.arg1
+        ags = tm0.arg2 # list of arguments
+        if (pnm == "+"):
+            assert len(ags) == 2
+            tv1 = term_eval01(ags[0], env)
+            tv2 = term_eval01(ags[1], env)
+            print("TMopr: tv1 = " + str(tv1))
+            print("TMopr: tv2 = " + str(tv2))
+            assert tv1.ctag == "TVint"
+            assert tv2.ctag == "TVint"
+            return tval_int(tv1.arg1 + tv2.arg1)
+        if (pnm == "-"):
+            assert len(ags) == 2
+            tv1 = term_eval01(ags[0], env)
+            tv2 = term_eval01(ags[1], env)
+            assert tv1.ctag == "TVint"
+            assert tv2.ctag == "TVint"
+            return tval_int(tv1.arg1 - tv2.arg1)
+        if (pnm == "<="):
+            assert len(ags) == 2
+            tv1 = term_eval01(ags[0], env)
+            tv2 = term_eval01(ags[1], env)
+            assert tv1.ctag == "TVint"
+            assert tv2.ctag == "TVint"
+            return tval_btf(tv1.arg1 <= tv2.arg1)
+        raise TypeError(pnm) # HX-2025-06-03: unsupported!
+    if (tm0.ctag == "TMapp"):
+        tm1 = tm0.arg1
+        tv1 = term_eval01(tm1, env)
+        assert tv1.ctag == "TVclo"
+        tm2 = tm0.arg1
+        tv2 = term_eval01(tm2, env)
+        tmf = tv1.arg1
+        env = tv1.arg2
+        if tmf.ctag == "TMlam":
+            x01 = tmf.arg1
+            env = xenv_cons(x01, tv2, env)
+            return term_eval01(tmf.arg2, env)
+        raise TypeError(tmf) # HX-2025-06-03: type error!
+    if (tm0.ctag == "TMif0"):
+        tm1 = tm0.arg1
+        tv1 = term_eval01(tm1, env)
+        assert tv1.ctag == "TVbtf"
+        if tv1.arg1:
+            return term_eval01(tm0.arg2, env) # then
+        else:
+            return term_eval01(tm0.arg3, env) # else
+    raise TypeError(tm0) # HX-2025-05-27: should be deadcode!    
+
+##################################################################
+
+print("eval(int_1) = " + str(term_eval00(int_1)))
+print("eval(btf_t) = " + str(term_eval00(btf_t)))
+print("eval(int_1 + int_1) = " + str(term_eval00(term_add(int_1, int_1))))
+print("eval(int_1 - int_1) = " + str(term_eval00(term_sub(int_1, int_1))))
+print("eval(int_1 <= int_1) = " + str(term_eval00(term_lte(int_1, int_1))))
+
+term_dbl = term_lam("x", term_add(var_x, var_x))
+print("eval(term_dbl(int_1)) = " + str(term_eval00(term_app(term_dbl, int_1))))
+
+##################################################################
+
